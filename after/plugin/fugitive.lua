2d6e553e6e04a4691ccd2903e6d5448067dbe440
Mohammad Maaita <almaaitamohammad@gmail.com>
2024-01-23 01:36:26 +0300

    fix: add fugitive toggle function

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,11 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+function toggle()
+  local fugitive_buf_no = vim.fn.bufnr('^fugitive:')
+  local buf_win_id = vim.fn.bufwinid(fugitive_buf_no)
+  if fugitive_buf_no >= 0 and buf_win_id >= 0 then
+    print('closing fugitive window')
+    vim.api.nvim_win_close(buf_win_id, false)
+  else
+    vim.cmd(":G")
+  end
+end
+vim.keymap.set("n", "<leader>gs", toggle);
