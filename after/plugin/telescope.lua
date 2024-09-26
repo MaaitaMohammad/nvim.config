286f036c12003f403e78aeded7c142ad34af2103
MaaitaMohammad <almaaitamohammad@gmail.com>
2024-09-26 11:12:32 +0300

    integrate nvim-tree with telescope to enable find_files and live_grep on a subset of files and directories

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,24 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<leader>pf", function()
+	local node = require("nvim-tree.api").tree.get_node_under_cursor()
+	if node then
+		return builtin.find_files({
+			search_dirs = { node.absolute_path },
+		})
+	else
+		return builtin.find_files()
+	end
+end, {})
+
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+
+vim.keymap.set("n", "<leader>ps", function()
+	local node = require("nvim-tree.api").tree.get_node_under_cursor()
+	if node then
+		return builtin.live_grep({
+			search_dirs = { node.absolute_path },
+		})
+	else
+		return builtin.live_grep()
+	end
+end, {})
